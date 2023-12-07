bbe28ab1ac1de6bb9cf824c3f8a677d2e3d07924
batuberque <123419583+batuberque@users.noreply.github.com>
2023-12-07 22:14:31 +0300

    frontend-first-commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Post from "./Post";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/post">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/post" element={<Post />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("no container to render to");
+}
+
+const root = createRoot(container);
+root.render(<App />);
---
+++ frontend/src/Post.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+type Selamla = {
+  selam?: string;
+};
+
+const Post = () => {
+  const [selam, setSelam] = useState<Selamla>({ selam: "selamın aleyküm" });
+
+  const selamla = () => {
+    setSelam({ selam: "aleyküm selam" });
+  };
+
+  return (
+    <div>
+      <h1 className="h1">{selam?.selam || "Değer yok"}</h1>
+      <button onClick={selamla}>SELAMLA!</button>
+    </div>
+  );
+};
+
+export default Post;
