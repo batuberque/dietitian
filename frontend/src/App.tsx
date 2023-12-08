e4a3ef894d2c8a7573c646c6c391fc1a0d9b7fc4
batuberque <123419583+batuberque@users.noreply.github.com>
2023-12-08 20:54:59 +0300

    connected-backend-frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./services/query-client";
 import Post from "./components/Post/Post";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    },
-  },
-});
 
 const App = () => {
   return (
--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,23 +1,81 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  Post,
+  fetchPosts,
+  addPost,
+  updatePost,
+  deletePost,
+} from '../../services/queries';
 
-type Selamla = {
-  selam?: string;
-};
+const Post: React.FC = () => {
+  const queryClient = useQueryClient();
+  const [newPost, setNewPost] = useState({ title: '', content: '' });
+  const [editingPost, setEditingPost] = useState<Post | null>(null);
 
-const Post = () => {
-  const [selam, setSelam] = useState<Selamla>({ selam: "selamın aleyküm" });
+  const { data: post, isLoading } = useQuery(['post'], fetchPosts);
 
-  const selamla = () => {
-    setSelam({
-      selam:
-        selam.selam === "aleyküm selam" ? "selamın aleyküm" : "aleyküm selam",
-    });
+  const addMutation = useMutation(addPost, {
+    onSuccess: () => {
+      void queryClient.invalidateQueries(['post']);
+      setNewPost({ title: '', content: '' });
+    },
+  });
+
+  const updateMutation = useMutation(updatePost, {
+    onSuccess: () => {
+      void queryClient.invalidateQueries(['post']);
+      setEditingPost(null);
+    },
+  });
+
+  const deleteMutation = useMutation(deletePost, {
+    onSuccess: () => queryClient.invalidateQueries(['post']),
+  });
+
+  const handleAddOrUpdatePost = () => {
+    if (editingPost) {
+      updateMutation.mutate({ postId: editingPost._id!, updatedData: newPost });
+    } else {
+      addMutation.mutate(newPost);
+    }
   };
+
+  const handleEditPost = (post: Post) => {
+    setEditingPost(post);
+    setNewPost({ title: post.title, content: post.content });
+  };
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1 className="h1">{selam?.selam || "Değer yok"}</h1>
-      <button onClick={selamla}>SELAMLA!</button>
+      <input
+        type="text"
+        placeholder="Title"
+        value={newPost.title}
+        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        value={newPost.content}
+        onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
+      />
+      <button onClick={handleAddOrUpdatePost}>
+        {editingPost ? 'Update Post' : 'Add Post'}
+      </button>
+
+      {post?.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <button onClick={() => handleEditPost(post)}>Edit</button>
+          <button onClick={() => deleteMutation.mutate(post._id!)}>
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
