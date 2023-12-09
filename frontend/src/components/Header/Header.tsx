e343d2748ce791f14ea9fa3672e9fa065f911034
batuberque <123419583+batuberque@users.noreply.github.com>
2023-12-09 20:21:40 +0300

    add-tailwindcss-configure-dockerproblems

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
 
-import './header.css';
-
 const Header: React.FC = () => {
   return (
-    <div className="header">
-      <div className="logo">
-        <span>
-          <Link to={'/'}>DİYETİSYEN UYGULAMASI</Link>
-        </span>
-      </div>
-      <div className="titles">
-        <span>
-          <Link to={'/post'}>Postlar</Link>
-        </span>
-        <span>Sayfalar</span>
+    <div className="header bg-gray-100 p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="logo">
+          <span className="text-xl font-bold text-gray-700">
+            <Link to={'/'}>DİYETİSYEN UYGULAMASI</Link>
+          </span>
+        </div>
+        <div className="titles flex gap-4">
+          <span className="text-lg text-gray-600 hover:text-gray-800 transition duration-300">
+            <Link to={'/post'}>Postlar</Link>
+          </span>
+          <span className="text-lg text-gray-600 hover:text-gray-800 transition duration-300">
+            Sayfalar
+          </span>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -2,11 +2,12 @@
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchPosts, addPost, deletePost } from '../../services/queries';
-import './post.css';
+import { Link } from 'react-router-dom';
 
 const PostComponent: React.FC = () => {
   const queryClient = useQueryClient();
   const [newPost, setNewPost] = useState({ title: '', content: '' });
+  const [addedPostId, setAddedPostId] = useState<string | null>(null);
 
   const { data: post, isLoading } = useQuery(['post'], fetchPosts);
 
@@ -14,7 +15,7 @@ const PostComponent: React.FC = () => {
     onSuccess: (data) => {
       void queryClient.invalidateQueries(['post']);
       setNewPost({ title: '', content: '' });
-      alert(`Post added with ID: ${data._id}`);
+      setAddedPostId(data._id);
     },
   });
 
@@ -26,40 +27,63 @@ const PostComponent: React.FC = () => {
     addMutation.mutate(newPost);
   };
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
 
   return (
-    <div className="first-div">
-      <input
-        className="text"
-        type="text"
-        placeholder="Başlık"
-        value={newPost.title}
-        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-      />
-      <input
-        className="content"
-        type="text"
-        placeholder="İçerik"
-        value={newPost.content}
-        onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
-      />
-      <button onClick={handleAddOrUpdatePost}>{'EKLEME BUTONU'}</button>
+    <div className="flex flex-col items-center justify-center my-10">
+      <div className="mb-5">
+        <input
+          className="p-2 border border-gray-300 rounded mr-2"
+          type="text"
+          placeholder="Başlık"
+          value={newPost.title}
+          onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+        />
+        <input
+          className="p-2 border border-gray-300 rounded"
+          type="text"
+          placeholder="İçerik"
+          value={newPost.content}
+          onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
+        />
+        <button
+          className="p-2 bg-blue-500 text-white rounded ml-2"
+          onClick={handleAddOrUpdatePost}
+        >
+          EKLEME BUTONU
+        </button>
+      </div>
 
       {post?.map((post) => (
-        <div key={post._id}>
-          <h3>{post.title}</h3>
-          <h3>
-            <a href={`/post/${post._id}`}>{post.title}</a>
-          </h3>
-          <p>{post.content}</p>
-          <button>Edit</button>
-          <button
-            className="button"
-            onClick={() => deleteMutation.mutate(post._id!)}
-          >
-            Delete
-          </button>
+        <div
+          key={post._id}
+          className="p-4 border border-gray-300 rounded mb-4 max-w-md h-[200px] overflow-auto"
+        >
+          <h3 className="text-lg font-bold truncate">{post.title}</h3>
+          <p className="text-sm truncate">{post.content}</p>
+          <div className="flex justify-end mt-3">
+            <button className="text-blue-500 hover:text-blue-700 mr-2">
+              Edit
+            </button>
+            <button
+              className="text-red-500 hover:text-red-700"
+              onClick={() => deleteMutation.mutate(post._id!)}
+            >
+              Delete
+            </button>
+          </div>
+          <div>
+            {addedPostId === post._id && (
+              <div>
+                <Link to={`/post/${post._id}`}>Post ID: {post._id}</Link>{' '}
+              </div>
+            )}
+          </div>
         </div>
       ))}
     </div>
