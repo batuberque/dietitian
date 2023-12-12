f6ed73c7f996dc3484f6e9228553a5206b30d09b
batuberque <123419583+batuberque@users.noreply.github.com>
2023-12-12 16:34:15 +0300

    added-contact

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("./mongoose-connection");
 
-const postRouter = require("./routes/post");
 const indexRouter = require("./routes/index");
+const postRouter = require("./routes/post");
+const contactRouter = require("./routes/contact");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/post", postRouter);
 app.use("/", indexRouter);
+app.use("/post", postRouter);
+app.use("/contact", contactRouter);
 
 module.exports = app;
